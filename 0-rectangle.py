4aaae06e63d54e717e2481d5bd2ab47eba342e35
hammad-ibrahim <95659983+hammad-ibrahim@users.noreply.github.com>
2024-04-10 15:03:47 +0200

    Delete 0-rectangle.py
    deleting unneeded files

--- 0-rectangle.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# 0-rectangle.py
-# Hammad-ibrahim  <hammad-ibrahim@alxschool.com>
-"""Defines a Rectangle class."""
-
-
-class Rectangle:
-    """Represent a rectangle."""
-    pass
